c0ba432c3438735952318eaaeb06b428363317e3
Steffen A. Mork <linux-dev@morknet.de>
2023-03-23 10:21:04 +0100

    Corrected generated SPDX header.

--- SctCppTimer/src-gen/QTimerStatechart.h
+++ SctCppTimer/src-gen/QTimerStatechart.h
@@ -1,6 +1,6 @@
 /* #
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: Copyright (C) 2022 Steffen A. Mork
+# SPDX-FileCopyrightText: Copyright (C) 2022-2023 Steffen A. Mork
 # */
 
 #ifndef QTIMERSTATECHART_H_
